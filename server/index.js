fc91011c8bb4e791c3d538c3a37fcb196226a585
Kevin <kqd4ng@gmail.com>
2018-10-22 18:12:47 -0500

    test

--- server/index.js
+++ server/index.js
@@ -9,21 +9,34 @@ const app = express();
 
 // UNCOMMENT FOR REACT
 // app.use(express.static(__dirname + '/../react-client/dist'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // UNCOMMENT FOR ANGULAR
 app.use(express.static(path.join(__dirname, '/../angular-client')));
 app.use(express.static(path.join(__dirname, '/../node_modules')));
-
+console.log('hello');
 app.get('/items', (req, res) => {
-  items.selectAll((err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+  // items.selectAll((err, data) => {
+  //   if (err) {
+  //     res.sendStatus(200);
+  //   } else {
+  //     res.json(data);
+  //   }
+  // });
 });
+// app.get('/api/activity/', (req, res) => {
+//   items.selectAll((err, data) => {
+//     if (err) {
+//       res.sendStatus(200);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
+// app.post('/items', (req, res) => {
+//   console.log(req.body);
+// });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port 3000!');
 });
